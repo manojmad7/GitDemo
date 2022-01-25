0d2d3dbc842918ddf6c642da8e3a535315bd2297
Manoj Pruthvi <manojpruthvim@gmail.com>
2022-01-25 16:50:21 +0530

    New Code

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\browsers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		System.out.println("Am the new code");
+		System.out.println("Am the new code2");
+	}
+
+}
---
+++ src/Java_Basics/Basics1.java
@@ -0,0 +1,46 @@
+package Java_Basics;
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		//Company.tcs();
+		Company c = new Company();
+		System.out.println(c.sector);
+		c.tcs();
+		
+		Atos a = new Atos();
+		a.id=100;
+		System.out.println(a.id);
+		a.fedex("workday", 45);
+		
+		
+		
+		
+	}
+}
+class Company
+{		
+		String sector = "IT";
+		
+		public void tcs()
+		{
+			String name = "Manoj";
+			int age = 24;
+			int salary = 40;
+			System.out.println("Name is "+name);
+			System.out.println("Age is "+age);
+			System.out.println("Salary is "+salary);
+			
+		}
+}
+class Atos
+{
+	int id;
+	static void fedex(String prjct, int membrs)
+	{
+		System.out.println("Project is "+prjct);
+		System.out.println("No. of members in workday "+membrs);
+	}
+}
+
+
